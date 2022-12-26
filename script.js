948bc8e8fe2f046a4a538b416d26a51edf731eba
Mabadou Witunge Kayumba <114286328+Mkayumba@users.noreply.github.com>
2022-12-26 13:34:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var names=new Array();
-names[0]="Kayumba";
-names[1]="Joshua";
-names[2]="January";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
 names[5]="frank";
